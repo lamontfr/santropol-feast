705ae1c106f50812da0fd349f493d1cb61beb982
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2016-05-13 18:29:21 -0400

    Implement mark_as_read function and tests
    Issue #109
    
    Signed-off-by: Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>

--- django/santropolFeast/member/urls.py
+++ django/santropolFeast/member/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns(
         ClientList.as_view(), name='list'),
     url(_(r'^notes/$'),
         NoteList.as_view(), name='notes'),
+    url(_(r'^note/read/(?P<id>[0-9]{1})/$'),
+        mark_as_read, name='read'),
 )
